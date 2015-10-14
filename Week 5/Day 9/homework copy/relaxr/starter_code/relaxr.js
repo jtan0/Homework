318f3efa69d3547ac3aab85c1e4705862d126dd1
jtan0 <jacqueline.tan0@gmail.com>
2015-10-13 23:12:31 -0400

    Javascript
    Added javascript file

--- Week 5/Day 9/homework copy/relaxr/starter_code/relaxr.js
+++ Week 5/Day 9/homework copy/relaxr/starter_code/relaxr.js
@@ -1,5 +1,18 @@
-$(document).ready(function(event) {
-	event.preventDefault();
+$(document).ready(function(e) {
 	$('.readmore').click(function(){
-		$('.hide').addClass('')
-});
+		$('#show-this-on-click').slideDown('slow');
+		$('.readless').show();
+		$('.readmore').hide();
+	});
+
+	$('.readless').click(function() {
+		$('#show-this-on-click').slideUp('slow');
+		$('.readless').hide();
+		$('.readmore').show();
+	});
+
+	$('.learnmore').click(function() {
+		$('.hide').slideDown('slow');
+		$('.learnmore').hide();
+	});
+})
