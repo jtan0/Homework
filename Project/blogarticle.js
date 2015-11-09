23c6acea51fd168148371adc07ec6e35b0903fc0
jtan0 <jacqueline.tan0@gmail.com>
2015-11-09 18:43:15 -0500

    added final project

---
+++ Project/blogarticle.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	var firebaseRef = new Firebase("https://radiant-torch-3037.firebaseio.com/");
+
+var postComment = function() {
+  var name = document.getElementById("name").value,
+      comment = document.getElementById("comment").value;
+  
+  if (name && comment) {
+      firebaseRef.push({
+      name: name,
+      comment: comment
+    });
+  }
+  document.getElementById("name").value = '';
+  document.getElementById("comment").value = '';
+};
+
+firebaseRef.on("child_added", function(snapshot) {
+  var comment = snapshot.val();
+  addComment(comment.name, comment.comment);
+});
+
+var addComment = function(name, comment) {
+  var comments = document.getElementById("comments");
+  comments.innerHTML = "<hr><h4>" + name + " says:</h4><p>" + comment + "</p>" + comments.innerHTML;
+};
+});
